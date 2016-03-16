18566fd432beea29de96effc82aa3f59df60359f
Mike Tang <mike@mikecoder.cn>
2016-03-16 16:32:34 +0800

    init commit

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+Usual Work Shell
+---
+
+This is a collection for me to deal with my usual work.
+
+####SHELL LIST
+
+1. [checkWork.sh](./checkWork.sh) check your daily coding work in a git project.
+
+####LICENSE
+MIT LICENSE
+
---
+++ checkWork.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+user=$1
+echo $user
+git log --author="$(git config --get user.name)" --pretty=tformat: --numstat | gawk '{ add += $1 ; subs += $2 ; loc += $1 - $2 } END { printf "added lines: %s removed lines : %s total lines: %s\n",add,subs,loc }' -
